4318aa1b98dab93877ba5494d2ebe551b4cbba70
Ben Seligman <seligman.ben@gmail.com>
2013-10-07 13:03:41 -0400

    Update readme. Remove unused function from application_controller.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todos
+* Todo app created in rails and backbone.
+* Uses jquery-ui sortable to reorder notes.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,6 @@ class ApplicationController < ActionController::Base
     redirect_to new_session_url unless self.logged_in?
   end
 
-  def require_owning_user(todo_item)
-    raise "you aren't authorized to change that note" unless todo_item.user == self.current_user.id
-  end
-
   def require_no_logged_in_user
     redirect_to root_url if self.logged_in?
   end
